f02a2ea8d6fd4dee9d8897578fc9827ec5df1604
Sudarshan <poudelsudarshan650@gmail.com>
2024-07-22 18:32:47 +0545

    Slight change in Backend

--- src/assets/Data/backend_data.js
+++ src/assets/Data/backend_data.js
@@ -1,37 +1,30 @@
-export const fetchPlaces = async () => {
+fetchData = async (url) => {
     try {
-        const response = await fetch("http://127.0.0.1:5000/data/places");
+        const response = await fetch(url);
         const data = await response.json();
-        console.log(data)
         return data;
     } catch (error) {
         console.error("Error fetching data:", error);
         return null;
     }
 };
+
+export const fetchPlaces = async () => {
+    return fetchData('http://127.0.0.1:5000/data/places')
+};
+
 
 export const fetchCities = async () => {
-    try {
-        const response = await fetch("http://127.0.0.1:5000/data/cities");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return null;
-    }
+    return fetchData('http://127.0.0.1:5000/data/cities')
 };
+
 
 export const fetchAccommodations = async () => {
-    try {
-        const response = await fetch("http://127.0.0.1:5000/data/accmmodations");
-        const data = await response.json();
-        return data;
-    } catch (error) {
-        console.error("Error fetching data:", error);
-        return null;
-    }
+    return fetchData('http://127.0.0.1:5000/data/accmmodations')
 };
 
+
+// Code to use in components :
 
 // const [places, setPlaces] = useState([]);
 
