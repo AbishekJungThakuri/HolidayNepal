0500034ad4f5b0f353e38880e77032fb3fb12aef
Sudarshan <poudelsudarshan650@gmail.com>
2024-07-24 07:11:56 +0545

    Places page css fixed

--- src/Components/Category/Category.jsx
+++ src/Components/Category/Category.jsx
@@ -1,42 +1,43 @@
-import React, { useContext } from 'react';
+import React, { useContext, useState } from 'react';
 import PropTypes from 'prop-types';
 import './Category.css';
 import { storeContext } from '../../Context/StoreContext';
 import { Placelayout } from '../PlaceLayoutPages/Placelayout';
-
+import { IoSearch } from "react-icons/io5";
 export const Category = ({image,Ltext,Utext,category}) => {
   
   const { places } = useContext(storeContext);
+  const [searchKeyword, changeSearch] = useState('')
 
+  const searchItems = () =>{
+    console.log(searchKeyword)
+  }
+  
   return (
     <div className="place-container">
       <div className="pamplet">
         <div className="pamplet-img">
           <img className='hero' src={image} alt="" />
         </div>
-        <h1>
+        <h1 className='pamplet-text'>
           {Utext} <br />
-          {Ltext}
+          <span>{Ltext}</span>
         </h1>
       </div>
 
-      <div className="sort-places">
-        <h1>Destinations</h1>
-        <p>Sort places by</p>
-        <select className="selectby" name="" id="">
-          <option value="popular">Most Popular</option>
-          <option value="expensive">Most Expensive</option>
-        </select>
-        <div className="place-list"></div>
+      <h1 className='heading'>Destinations</h1>
+      <div className="search-places">
+        <input id='searchBox' type="text" placeholder={'Search ' + category +'s'} value={searchKeyword} onChange={(e) => changeSearch(e.target.value)}/>
+        <button className='search_btn' onClick={searchItems()}><IoSearch /></button>
       </div>
 
-      <div className="places-loadmore">
         <div className="places">
           {places.map((place, index) => (
               category === place.category ? (
               <div key={index}>
                 <Placelayout
                   name={place.name}
+                  id={place.id}
                   img={place.image}
                   rating={place.rating}
                   price={place.price}
@@ -46,10 +47,6 @@ export const Category = ({image,Ltext,Utext,category}) => {
             ) : null
           ))}
         </div>
-      </div>
-      <div className='load-more'>
-        <button className="load-btn">Load more</button>
-      </div>
     </div>
   );
 };
--- src/Components/PlaceLayoutPages/Placelayout.jsx
+++ src/Components/PlaceLayoutPages/Placelayout.jsx
@@ -1,28 +1,26 @@
 import React from 'react'
 import './Placelayout.css'
-import { FaStar } from "react-icons/fa";
 import { IoHeartCircle } from "react-icons/io5";
+import { FaStar } from "react-icons/fa6";
 
 
-export const Placelayout = ({name,price,rating,img,type}) => {
+export const Placelayout = ({name,id, price,rating,img,type}) => {
   return (
     <div className='place-layout'>
-        <div className="place-imag">
-            <img src={img} alt="" />
+        <div className="place-image">
+            <img src={img} alt={name + 'image'} />
         </div>
+
         <div className="place-info">
-            <div className='name-heart' >
-               <h1> {name} </h1>
-               <IoHeartCircle className='heart' />
-            </div>
-            {
-              type === 'event' ? null :
-            <p>${price} starting</p>
-            }
+          <div className="top">
+          <h1 className='name'> {name} </h1>
+            <p>Historic | Cultural</p>
             <div className="star-rating">
             <FaStar className='star' />
             <p>{rating}</p>
             </div>
+          </div>
+            
             <button className='readmore'>read more</button>
         </div>
     </div>
